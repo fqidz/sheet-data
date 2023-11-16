8de7d21aa54942395814de9a90a2bad40fcceb9f
BuildTools <unconfigured@null.spigotmc.org>
2023-11-16 15:59:10 +0000

    fix progress 2

--- faidzprinting.py
+++ faidzprinting.py
@@ -75,7 +75,7 @@ with st.expander(label='Form', expanded=True):
     
     # initialize variables
     progress_text = 'Loading...'
-    progress = 0.01
+    progress = 0
 
     total_price = 0
     total_colored = 0
